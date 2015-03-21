27efa1e4e02e3f151cd4eef6168a45737fb221bc
Samyak Bhuta <samyak.bhuta@gmail.com>
2015-03-21 16:53:36 +0530

    Adds the tournament name.

--- Cricket.playground/section-1.swift
+++ Cricket.playground/section-1.swift
@@ -1,8 +1,9 @@
 var team1 = "India"
 var team2 = "Australia"
 var matchTitle = "second semi final"
+var tournamentTitle = "ICC World Cup 2015"
 
-println("Welcome to the \(matchTitle). It is \(team1) vs. \(team2).")
+println("Welcome to the \(matchTitle) of \(tournamentTitle). It is \(team1) vs. \(team2).")
 
 
 
