4638e53d7b928c2cab7a05632d95b12760ae5452
Samyak Bhuta <samyak.bhuta@gmail.com>
2015-03-21 17:07:21 +0530

    adds weather report information

--- Cricket.playground/section-1.swift
+++ Cricket.playground/section-1.swift
@@ -3,7 +3,18 @@ var team2 = "Australia"
 var matchTitle = "second semi final"
 var tournamentTitle = "ICC World Cup 2015"
 
+// Opening text
 println("Welcome to the \(matchTitle) of \(tournamentTitle). It is \(team1) vs. \(team2).")
+
+// Weather Report
+
+var temp = 32.5
+var wind = 32
+var windDirection = "north-south"
+var humidity = 70
+
+println("Temperature: \(temp) Degree Celcius, Wind : \(wind) kph \(windDirection), Moisture : \(humidity)% Humid")
+
 
 
 
