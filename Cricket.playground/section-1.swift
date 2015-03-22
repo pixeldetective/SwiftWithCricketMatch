f90bb9bd972b0dc937b42c4160fe09f3c3d7ff68
pixeldetective <khamoshpathak@gmail.com>
2015-03-22 19:27:50 +0530

    Adds If Else statement for determinig the winning captain.

--- Cricket.playground/section-1.swift
+++ Cricket.playground/section-1.swift
@@ -92,3 +92,46 @@ if ( humidity < 40 ) {
 // This outputs the final, humanized sentence.
 
 println("It's \(tempHumamReadable), \(windHumamReadable) and \(humidityHumamReadable) day today.")
+
+
+
+// MS Dhoni and Micheal Clark are on their way to the toss.
+// Micheal Clark tossed the coin. MS Dhoni called for Heads.
+// Heads it is. MS Dhoni won the toss. 
+// It's Tails. Micheal Clark won the toss. 
+// MS Dhoni decided to bat first.
+// MS Dhono decided to bowl first. 
+
+// *Captain 1* and *Captain 2* are on their way to the toss.
+// *Captain 1 or 2* tossed the coin. *Captain 1 or 2* called for *heads or tails*.
+// *Captain 1 or 2* won the toss. *Captain 1 or 2* decided to *Bat or Bowl* first.
+
+var captain1 = "MS Dhoni"
+var captain2 = "Micheal Clark"
+var tossingCaptain = "MS Dhoni"
+var tossCallingCaptain = "Micheal Clark"
+var tossCalled = "heads"
+var tossActual = "heads"
+var winningCaptain = ""
+
+
+println("\(captain1) from \(team1) and \(captain2) from \(team2) are on their way to the toss.")
+println("\(tossingCaptain) tossed the coin. \(tossCallingCaptain) called for \(tossCalled).")
+println("It's \(tossActual).")
+
+// If actual toss is toss called, toss calling captain is the winner. Else, tossing captain is the winner.
+
+if ( tossCalled == tossActual) {
+    winningCaptain = tossCallingCaptain
+}
+
+else {
+    winningCaptain = tossingCaptain
+}
+
+
+ println("\(winningCaptain) won the toss.")
+
+
+
+
